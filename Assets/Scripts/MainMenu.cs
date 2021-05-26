c3c0d092f4f3417c68514fda3fd69ea40dcdb94d
ose <jarg_8@hotmail.com>
2021-05-26 23:14:46 +0200

    Main Menu, level 1 and 2

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+
+    }
+}
